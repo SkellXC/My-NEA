64e3f9bec2a594001cb6c1f2aea1164a21b93626
Ethan Travasso <flameboyepic@gmail.com>
2025-02-04 23:26:10 +0000

    added buttons. addExerciseButton needs to be in the right layer

--- gui.py
+++ gui.py
@@ -7,11 +7,37 @@ class App(tk.Tk):
         self.title("Workout Tracker")
         self.geometry("400x620")# UI size
 
+        # self.configure(bg="grey12") use if background isn't matching
 
-        self.container = tk.Frame(self)# creates a container to holder widgets
-        self.container.pack(fill=tk.BOTH, expand=True)
-        # Acts like a wrapper for the different pages
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)# Allow for horizontal streching
 
+        
+
+        self.topBar = tk.Frame(self,height=50, bg="grey9")# creates a top bar
+        self.topBar.grid(row=0, column = 0, sticky="nsew")# Sticks it to the top but lets it stretch to the width
+
+        self.bottomBar = tk.Frame(self,height=75, bg="grey9")# Copy of above for bottom
+        self.bottomBar.grid(row=2,column=0, sticky="nsew")
+
+        self.container = tk.Frame(self, bg="grey12")# creates a container to holder widgets
+        self.container.grid(row=1,column=0, sticky="nsew")
+
+        # buttons should be after container
+
+        self.homeButton = tk.Button(self.topBar, text="Home", bg="grey9", fg="white", font=("Arial", 12))
+        self.homeButton.pack(side="left", padx=10)
+
+
+        
+        self.addExerciseButton = tk.Button(
+            self.container, text="+", font=("Arial", 20, "bold"), 
+            bg="grey9", fg="white", width=3, height=1, 
+            relief="ridge", borderwidth=5)# Border styling
+
+        #self.addExerciseButton.grid(row=0, column=0,pady=500,padx=170)  # Position of the button
+        self.addExerciseButton.grid(row=0, column=0, pady=500, padx=170)
+        self.addExerciseButton.lift()
         self.frames = {}                      
 
     def show_frame(self, page):
@@ -20,6 +46,7 @@ class App(tk.Tk):
         # This function rasies the selected frame to the
         # front of the window.
 
+    
 
 
 
