3a767ae9120c7ced8d516e851efc2d5092bde8c6
Ethan Travasso <flameboyepic@gmail.com>
2025-02-07 20:53:33 +0000

    Added the framework for different pages

--- gui.py
+++ gui.py
@@ -25,33 +25,56 @@ class App(tk.Tk):
 
         # buttons should be after container
 
+
+
+
+
+
+
+        self.frames = {}
+
+        for F in (HomePage, Settings):# Loop through the pages
+            frame = F(self.container, self) # Creates an instance of the frame
+            self.frames[F] = frame # Then stores it in the dictionary
+            frame.grid(row=0, column=0, sticky="nsew") # Stacks the frames back
+        
+        self.show_frame(HomePage)
+
         self.homeButton = tk.Button(self.topBar, text="Home",
-         bg="grey9", fg="white", font=("Arial", 12))
+         bg="grey9", fg="white", font=("Arial", 12),
+         command=lambda: self.show_frame(HomePage))
+        
         self.homeButton.pack(side="left", padx=10)
 
-
-        
-        self.addExerciseButton = tk.Button(
-            self.bottomBar, text="+", font=("Arial", 20, "bold"), 
-            bg="grey9", fg="white", width=3, height=1, 
-            relief="ridge", borderwidth=5)# Border styling
-        self.addExerciseButton.pack(pady=10)# pady raises it from the bottom a bit
-
         self.settingsButton = tk.Button(self.topBar, text="Settings",
-         bg="grey9", fg="white", font=("Arial", 12))
+         bg="grey9", fg="white", font=("Arial", 12),
+         command=lambda: self.show_frame(Settings)
+         )
+        
         self.settingsButton.pack(side="right",padx=10)
-
-
-        self.frames = {}                      
 
     def show_frame(self, page):
         frame = self.frames[page]
-        frame.tkraise()
-        # This function rasies the selected frame to the
-        # front of the window.
+        frame.tkraise()# Display the frame
+
 
     
+    
+class HomePage(tk.Frame):
+    def __init__(self,parent,controller):
+        super().__init__(parent,bg="grey12")
+        label = tk.Label(self, text="Home", font=("Arial", 24), bg="grey12", fg="white")
+        label.pack(side="top", pady=20,padx=150)
 
+class Settings(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg="grey12")  # Create a frame inside parent (container)
+
+        # Page title
+        label = tk.Label(self, text="Settings", font=("Arial", 24), bg="grey12", fg="white")
+        label.pack(pady=20,padx=150)  # Add some space
+
+        # Button to switch back to Home Page
 
 
 if __name__ == "__main__":
