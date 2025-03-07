59864b12bf7fa20e2b4f9cff998ea42dad0df3dc
Ethan Travasso <85579428+SkellXC@users.noreply.github.com>
2025-03-07 12:54:31 +0000

    Finished the exercise page
    Completed the exercise page and implemented it.

--- gui.py
+++ gui.py
@@ -54,40 +54,40 @@ class App(tk.Tk):
 
 
         
-        self.show_frame(HomePage)
+        self.showFrame(HomePage)
 
         self.homeButton = tk.Button(self.topBar, text="Home",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: self.show_frame(HomePage))
+         command=lambda: self.showFrame(HomePage))
         self.homeButton.pack(side="left", padx=10)
 
 
         self.settingsButton = tk.Button(self.topBar, text="Settings",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: self.show_frame(Settings))
+         command=lambda: self.showFrame(Settings))
         self.settingsButton.pack(side="right",padx=10)
 
         self.addExerciseButton = tk.Button(
             self.bottomBar, text="+", font=("Arial", 20, "bold"), 
             bg="grey9", fg="white", width=3, height=1, 
             relief="ridge", borderwidth=5,# Border styling
-            command=lambda: self.show_frame(ExerciseList))
+            command=lambda: self.showFrame(ExerciseList))
         self.addExerciseButton.pack(pady=10)# pady raises it from the bottom a bit
 
 
 
 
-    def show_frame(self, page):
+    def showFrame(self, page):
         # If page is a string (exercise name)
         if isinstance(page, str) and page in self.exercises:
             # Check if we've already created this exercise page
             if page not in self.frames:
                 # Create the frame
-                exercise_frame = ExercisePage(self.container, self, 
+                exerciseFrame = ExercisePage(self.container, self, 
                                              name=page, 
                                              group=self.exercises[page])# Create an instance of the page
-                self.frames[page] = exercise_frame
-                exercise_frame.grid(row=0, column=0, sticky="nsew")
+                self.frames[page] = exerciseFrame
+                exerciseFrame.grid(row=0, column=0, sticky="nsew")
             
             frame = self.frames[page]
         else:
@@ -125,17 +125,17 @@ class ExerciseList(tk.Frame):
         # Buttons for the exercise groups
         self.chestGroup = tk.Button(self,text="Chest",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(ChestGroup))
+         command=lambda: controller.showFrame(ChestGroup))
         self.chestGroup.pack(pady=10)# pady raises it from the bottom a bit
 
         self.BackGroup = tk.Button(self,text="Back",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(BackGroup))
+         command=lambda: controller.showFrame(BackGroup))
         self.BackGroup.pack(pady=10)# pady raises it from the bottom a bit
 
         self.LegsGroup = tk.Button(self,text="Legs",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(LegsGroup))
+         command=lambda: controller.showFrame(LegsGroup))
         self.LegsGroup.pack(pady=10)# pady raises it from the bottom a bit
 
 """
@@ -153,27 +153,27 @@ class ChestGroup(tk.Frame):
 
         self.BackButton = tk.Button(self, text="Back",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(ExerciseList))
+         command=lambda: controller.showFrame(ExerciseList))
         self.BackButton.pack(side="top",pady=5)
 
         self.benchPressBb = tk.Button(self, text="Barbell Bench Press",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame("Barbell Bench Press"))
+         command=lambda: controller.showFrame("Barbell Bench Press"))
         self.benchPressBb.pack(side="top",pady=10,padx=30)
         
         self.benchPressDb = tk.Button(self, text="Dumbbell Bench Press",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Dumbbell Bench Press"))
         self.benchPressDb.pack(side="top",pady=10)
 
         self.pushUps = tk.Button(self, text="Push-Ups",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Push-Ups"))
         self.pushUps.pack(side="top",pady=10)
 
         self.pecFly = tk.Button(self, text="Pec Fly",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Pec Fly"))
         self.pecFly.pack(side="top",pady=10)
 
 
@@ -187,28 +187,28 @@ class BackGroup(tk.Frame):
 
         self.BackButton = tk.Button(self, text="Back",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(ExerciseList))
+         command=lambda: controller.showFrame(ExerciseList))
         self.BackButton.pack(side="top",pady=5)
 
         self.deadlift = tk.Button(self, text="Deadlift",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Deadlift"))
         self.deadlift.pack(side="top",pady=10)
         
 
         self.latPulldown = tk.Button(self, text="Lat Pulldown",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Lat Pulldown"))
         self.latPulldown.pack(side="top",pady=10)
 
         self.lowRow = tk.Button(self, text="Low Row",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Low Row"))
         self.lowRow.pack(side="top",pady=10)
 
         self.barbellRow = tk.Button(self, text="Barbell Row",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Barbell Row"))
         self.barbellRow.pack(side="top",pady=10)
 
 
@@ -222,27 +222,27 @@ class LegsGroup(tk.Frame):
 
         self.BackButton = tk.Button(self, text="Back",
          bg="grey9", fg="white", font=("Arial", 12),
-         command=lambda: controller.show_frame(ExerciseList))
+         command=lambda: controller.showFrame(ExerciseList))
         self.BackButton.pack(side="top",pady=5)
 
         self.barbellSquats = tk.Button(self, text="Barbell Squats",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Barbell Squats"))
         self.barbellSquats.pack(side="top",pady=10)
 
         self.splitSquats = tk.Button(self, text="Split Squats",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Split Squats"))
         self.splitSquats.pack(side="top",pady=10)
 
         self.legRaises = tk.Button(self, text="Leg Raises",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Leg Raises"))
         self.legRaises.pack(side="top",pady=10)
 
         self.hamstringCurls = tk.Button(self, text="Hamstring Curls",
          bg="grey9", fg="white", font=("Arial", 14),
-         command=lambda: controller.show_frame(HomePage))
+         command=lambda: controller.showFrame("Hamstring Curls"))
         self.hamstringCurls.pack(side="top",pady=10)
 
 
@@ -251,7 +251,7 @@ class LegsGroup(tk.Frame):
 2.	Loop through the dictionary and create instances of exercise page for each exercise
 3.	Store each of these in self.frames 
 4.	Map the pages over
-5.	Open them with show_frame
+5.	Open them with showFrame
 """
 
 class ExercisePage(tk.Frame):
@@ -263,6 +263,12 @@ class ExercisePage(tk.Frame):
         exercisePageLabel = tk.Label(self, text=self.name, font=("Arial", 24), bg="grey12", fg="white")
         exercisePageLabel.pack(side="top", pady=20,padx=0)# Places a label with the exercise name at the top
 
+        """
+        Weights & Repetitions Textbox, increment button and decrement button.
+        I have made smaller frames to put these in to help organize.
+        """
+
+        # Textbox
         weightInputFrame = tk.Frame(self, bg="grey12", width=120, height=80)
         weightInputFrame.pack(side="top", pady=10)
         weightInputFrame.pack_propagate(False)  # Prevents the frame from resizing to fit its children
@@ -272,7 +278,21 @@ class ExercisePage(tk.Frame):
         self.weightInput.insert("1.0", "0","center")# Set 0 as the default value
         self.weightInput.pack(fill="both", expand=True)
         
+        # Buttons
+        weightFrame = tk.Frame(self, bg="grey12")
+        weightFrame.pack(side="top", pady=10)# Create a frame for the weight increment/decrement button
+        self.weightValue = tk.IntVar(value=0)# Default value
 
+        weightDecrement = tk.Button(weightFrame, text="-", font=("Arial", 20), command=self.decrementWeight)
+        weightDecrement.pack(side="left")
+        weightIncrement = tk.Button(weightFrame, text="+", font=("Arial", 20),command=self.incrementWeight)
+        weightIncrement.pack(side="left")  
+
+        """
+        Same funciton as above, just for the repetitions button
+        """
+
+        # Textbox
         repsInputFrame = tk.Frame(self, bg="grey12", width=120, height=80)# Create the textbox frame for reps
         repsInputFrame.pack(side="top", pady=10)
         repsInputFrame.pack_propagate(False)  
@@ -282,7 +302,41 @@ class ExercisePage(tk.Frame):
         self.repsInput.insert("1.0", "0","center")# Set 0 as the default value
         self.repsInput.pack(fill="both", expand=True)
 
+        # Buttons
+        repsFrame = tk.Frame(self, bg="grey12")
+        repsFrame.pack(side="top", pady=10)# Create a frame for the reps increment/decrement button
+        self.repsValue = tk.IntVar(value=0)# Default value
 
+        repsDecrement = tk.Button(repsFrame, text="-", font=("Arial", 20), command=self.decrementReps)
+        repsDecrement.pack(side="left")
+        repsIncrement = tk.Button(repsFrame, text="+", font=("Arial", 20),command=self.incrementReps)
+        repsIncrement.pack(side="left")       
+
+        # Save the set. Need to connect it to the database
+        saveButton = tk.Button(self, text="Save", font=("Arial", 20))
+        saveButton.pack(side="top", pady=15)
+
+    def incrementWeight(self):
+        self.updateTextbox(self.weightInput, 5)
+
+    def decrementWeight(self):
+        self.updateTextbox(self.weightInput, -5)
+
+    def incrementReps(self):
+        self.updateTextbox(self.repsInput, 5)
+
+    def decrementReps(self):
+        self.updateTextbox(self.repsInput, -5)
+
+    def updateTextbox(self, textbox, change):
+        try:
+            currentValue = int(textbox.get("1.0", "end").strip())
+        except ValueError:
+            currentValue = 0  # Default to 0 if invalid input
+        
+        newValue = max(0, currentValue + change)  # Prevents negative values
+        textbox.delete("1.0", "end")# Delete first line at the beginning of the text box
+        textbox.insert("1.0", str(newValue), "center")# Insert at the first line (hence 1.0)
 
 
 if __name__ == "__main__":# Convention that indicates that this file is meant to be run
